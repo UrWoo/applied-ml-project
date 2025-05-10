dc12f55ce64b2e952d69306e86160bf1fafd4790
UrWoo <4michalurban@gmail.com>
2025-05-10 21:37:08 +0200

    Added Discriminator for baseline model DCGAN, added weight initialization for DCGAN

--- models/DCGAN.py
+++ models/DCGAN.py
@@ -1,10 +1,11 @@
+import torch
 import torch.nn as nn
 
 class Generator(nn.Module):
     def __init__(self, noise_dim, final_conv_size, output_channels):
         super(Generator, self).__init__()
         self.neuralnet = nn.Sequential(
-            # First step: Project and reshape the initial noise vector
+            # First layer project and reshape the initial noise vector
             nn.Linear(noise_dim, final_conv_size * 8 * 4 * 4),
             nn.Unflatten(1, (final_conv_size * 8, 4, 4)),
             nn.ReLU(), 
@@ -31,3 +32,36 @@ class Generator(nn.Module):
 
     def forward(self, input):
         return self.neuralnet(input)
+    
+class Discriminator(nn.Module):
+    def __init__(self, first_conv_size, input_channels):
+        super(Discriminator, self).__init__()
+        self.neuralnet = nn.Sequential(
+            # First layer done without batch normalisation
+            nn.Conv2d(input_channels, first_conv_size, 4, 2, 1),
+            nn.LeakyReLU(0.2), 
+
+            self.layer(first_conv_size, first_conv_size * 2),
+
+            self.layer(first_conv_size * 2, first_conv_size * 4),
+
+            self.layer(first_conv_size * 4, first_conv_size * 8),
+
+            nn.Flatten(),
+            nn.Linear(first_conv_size * 8 * 4 * 4, 1),
+            nn.Sigmoid()
+        )
+    
+    def layer(self, input_channels, output_channels):
+        return nn.Sequential(
+            nn.Conv2d(input_channels, output_channels, 4, 2, 1),
+            nn.LeakyReLU(0.2),
+            nn.BatchNorm2d(output_channels)
+        )
+    
+    def forward(self, input):
+        return self.neuralnet(input)
+    
+def init_weights(m):
+    if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
