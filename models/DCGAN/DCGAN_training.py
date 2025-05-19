Created WGAN, fixed gradient issue in DCGAN

models/DCGAN/DCGAN_training.py
@@ -19,8 +19,8 @@ learning_rate = 0.0002
 beta_one = 0.5
 epochs = 50
 
-results_path = 'results'
-grid_path = 'results/grid'
+results_path = "results"
+grid_path = "results/grid"
 
 if not os.path.exists(results_path):
     os.makedirs(results_path)
@@ -41,7 +41,13 @@ dataset = ImageFolder(
 )
 
 # Set up dataloader
-dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=4)
+dataloader = DataLoader(
+    dataset=dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    pin_memory=True,
+    num_workers=4,
+)
 
 # Set up device to use accelerator when available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -107,16 +113,16 @@ for epoch in range(epochs):
         disc_loss = disc_real_loss + disc_fake_loss
 
         # Do a backwards pass
-        disc_opt.zero_grad()
+        discriminator.zero_grad()
         disc_loss.backward()
         disc_opt.step()
 
         # Train generator
         gen_disc_output = discriminator(fake_images).flatten()
         gen_loss = loss(gen_disc_output, disc_real_labels)
-        
+
         # Do a backwards pass
-        gen_opt.zero_grad()
+        generator.zero_grad()
         gen_loss.backward()
         gen_opt.step()
 
@@ -134,12 +140,12 @@ for epoch in range(epochs):
 
 # Print loss graph
 plt.title("Generator and Discriminator Losses")
-plt.plot(gen_losses,label="generator losses")
-plt.plot(disc_losses,label="discriminator losses")
+plt.plot(gen_losses, label="generator losses")
+plt.plot(disc_losses, label="discriminator losses")
 plt.xlabel("i")
 plt.ylabel("Loss")
 plt.legend()
-plt.savefig(os.path.join(results_path, 'loss_graph.png'))
+plt.savefig(os.path.join(results_path, "loss_graph.png"))
 plt.close()
 
 # Print progress on a fixed noise vector
@@ -147,10 +153,10 @@ for i, img in enumerate(images):
     grid = torchvision.utils.make_grid(img, padding=2, normalize=True)
 
     plt.figure()
-    plt.imshow(grid.permute(1,2,0).cpu().numpy())
-    plt.axis('off')
-    plt.savefig(os.path.join(grid_path, f'grid_epoch_{i}.png'))
+    plt.imshow(grid.permute(1, 2, 0).cpu().numpy())
+    plt.axis("off")
+    plt.savefig(os.path.join(grid_path, f"grid_epoch_{i}.png"))
     plt.close()
 
-torch.save(generator.state_dict(), 'generator.pth')
-torch.save(discriminator.state_dict(), 'discriminator.pth')
+torch.save(generator.state_dict(), "generator.pth")
+torch.save(discriminator.state_dict(), "discriminator.pth")

models/WGAN/WGAN.py
@@ -1,5 +1,5 @@
-import torch
 import torch.nn as nn
+
 
 class Generator(nn.Module):
     def __init__(self, noise_dim, final_conv_size, output_channels):
@@ -8,60 +8,53 @@ class Generator(nn.Module):
             # First layer project and reshape the initial noise vector
             nn.Linear(noise_dim, final_conv_size * 8 * 4 * 4),
             nn.Unflatten(1, (final_conv_size * 8, 4, 4)),
-            nn.ReLU(), 
+            nn.ReLU(),
             nn.BatchNorm2d(final_conv_size * 8),
-            
             # 3 Intermediate layers consisting of fractionally-strided convolutions, ReLU activation and batch normalisation
             self.layer(final_conv_size * 8, final_conv_size * 4),
-
             self.layer(final_conv_size * 4, final_conv_size * 2),
-
             self.layer(final_conv_size * 2, final_conv_size),
-
             # Final layer consisting of fractionally-strided convolution and Tanh activation function
             nn.ConvTranspose2d(final_conv_size, output_channels, 4, 2, 1),
-            nn.Tanh()
+            nn.Tanh(),
         )
 
     def layer(self, input_channels, output_channels):
         return nn.Sequential(
             nn.ConvTranspose2d(input_channels, output_channels, 4, 2, 1),
             nn.ReLU(),
-            nn.BatchNorm2d(output_channels)
+            nn.BatchNorm2d(output_channels),
         )
 
     def forward(self, input):
         return self.neuralnet(input)
-    
-class Discriminator(nn.Module):
+
+
+class Critic(nn.Module):
     def __init__(self, first_conv_size, input_channels):
-        super(Discriminator, self).__init__()
+        super(Critic, self).__init__()
         self.neuralnet = nn.Sequential(
             # First layer done without batch normalisation
             nn.Conv2d(input_channels, first_conv_size, 4, 2, 1),
-            nn.LeakyReLU(0.2), 
-
+            nn.LeakyReLU(0.2),
             self.layer(first_conv_size, first_conv_size * 2),
-
             self.layer(first_conv_size * 2, first_conv_size * 4),
-
             self.layer(first_conv_size * 4, first_conv_size * 8),
-
             nn.Flatten(),
             nn.Linear(first_conv_size * 8 * 4 * 4, 1),
-            nn.Sigmoid()
         )
-    
+
     def layer(self, input_channels, output_channels):
         return nn.Sequential(
             nn.Conv2d(input_channels, output_channels, 4, 2, 1),
             nn.LeakyReLU(0.2),
-            nn.BatchNorm2d(output_channels)
+            nn.BatchNorm2d(output_channels),
         )
-    
+
     def forward(self, input):
         return self.neuralnet(input)
-    
+
+
 def init_weights(m):
     if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
         nn.init.normal_(m.weight.data, 0.0, 0.02)